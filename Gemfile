a59afb846ef3d2dd8d6981690b5d4439ebddfb69
Yeison Osorio <yeison-arley25@hotmail.com>
2018-05-15 23:54:05 -0500

    db popuplation and adding charts resources

--- Gemfile
+++ Gemfile
@@ -55,3 +55,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'wicked'
+gem "chartkick"
+gem 'groupdate'
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+	House.create name: Faker::GameOfThrones.house,
+				 address: Faker::Address.street_address,
+				 stratum: rand(1..6)
+end
+
+SensorCategory.create name: "Energia",
+						description: "Categoria encargada de discriminar los sensores que son de corriente"
+
+SensorCategory.create name: "Agua",
+						description: "Categoria encargada de discriminar los sensores que son de agua"
+
+SensorCategory.create name: "Luminosidad",
+						description: "Categoria encargada de discriminar los sensores que son de luminosidad"
+
+
+
+5.times do |house|
+	5.times do |room| 
+		r = Room.create name: Faker::GameOfThrones.character,
+					house_id: house+1
+
+		2.times do
+			Sensor.create name: Faker::Commerce.product_name,
+						  room_id: r.id,
+						  comment: Faker::VForVendetta.quote,
+						  sensor_category_id: rand(1..3)
+		end
+	end
+end
